ef6a25d4b2bcd807a56ee7691c2e3adcd893a2dd
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2022-04-21 13:22:10 -0300

    changed headars axios config

--- src/services/api/config.ts
+++ src/services/api/config.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: { 'Access-Control-Allow-Origin': '*' },
   // headers: {
   //   Authorization: `Bearer ${process.env.API_KEY}`,
   // },
