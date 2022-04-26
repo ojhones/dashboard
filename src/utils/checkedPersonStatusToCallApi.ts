be947640c6cdbc2debc1de523d2d134cdb573dae
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2022-04-26 16:56:38 -0300

    fixed bug to call

--- src/utils/checkedPersonStatusToCallApi.ts
+++ src/utils/checkedPersonStatusToCallApi.ts
@@ -7,39 +7,23 @@ type PersonStatusProps = {
 export function checkedPersonStatusToCallApi(
   checkedPersonStatus: PersonStatusProps
 ) {
+  const carlos = [];
+
   if (!checkedPersonStatus) {
     return '';
   }
 
-  if (checkedPersonStatus.active === true) {
-    return `&status=2`;
+  if (checkedPersonStatus.active) {
+    carlos.push(2);
   }
 
-  if (checkedPersonStatus.pending === true) {
-    return `&status=1`;
+  if (checkedPersonStatus.pending) {
+    carlos.push(1);
   }
 
-  if (checkedPersonStatus.expired === true) {
-    return `&status=3`;
+  if (checkedPersonStatus.expired) {
+    carlos.push(3);
   }
 
-  if (checkedPersonStatus.active && checkedPersonStatus.pending) {
-    return `&status=1,2`;
-  }
-
-  if (checkedPersonStatus.active && checkedPersonStatus.expired) {
-    return `&status=2,3`;
-  }
-
-  if (checkedPersonStatus.pending && checkedPersonStatus.expired) {
-    return `&status=1,3`;
-  }
-
-  if (
-    checkedPersonStatus.active &&
-    checkedPersonStatus.expired &&
-    checkedPersonStatus.pending
-  ) {
-    return `&status=1,2,3`;
-  }
+  return `&status=${carlos.join(',')}`;
 }
